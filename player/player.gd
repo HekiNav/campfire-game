e8283a149dd64814494db6174ad7afddef37fb2f
Unto <developer.hekinav@gmail.com>
2026-02-28 17:25:47 +0200

    player turns

--- player/player.gd
+++ player/player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var SPEED = 150.0
 @export var JUMP_VELOCITY = -300.0
 @export var inventory: Inventory
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,6 +16,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor() and not global.is_menu_open:
 		velocity.y = JUMP_VELOCITY
 
+	if (velocity.x<0):
+		sprite_2d.flip_h = true
+	if (velocity.x>0):
+		sprite_2d.flip_h = false
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
