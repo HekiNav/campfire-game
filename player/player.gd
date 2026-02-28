7b5a3cf57867e9ce45be6ec3542a3a611d72235e
Unto <developer.hekinav@gmail.com>
2026-02-28 17:17:45 +0200

    plöayer movement fix

--- player/player.gd
+++ player/player.gd
@@ -12,15 +12,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not global.is_menu_open:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
+	if direction and not global.is_menu_open:
 		velocity.x = direction * SPEED
+	elif is_on_floor():
+		velocity.x = 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = velocity.x * 0.99
 	move_and_slide()
