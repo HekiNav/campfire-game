ecee421590a3e4dd2b0374487ee65416ef5abcfb
ropva <roopevarjus@gmail.com>
2026-02-28 14:12:31 +0200

    tweakin

--- map/TilemapManager.gd
+++ map/TilemapManager.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var frontground = self.get_node("MapBackground")
+@onready var foreground = self.get_node("MapForeground")
 @onready var background = self.get_node("MapBackground")
 
 
@@ -8,4 +8,11 @@ func _ready() -> void:
 	var coords: Vector2i
 	coords.x=0
 	coords.y=3
-	#set_cell(coords, source_id: int = -1, atlas_coords: Vector2i = Vector2i(-1, -1), alternative_tile: int = 0)
+	foreground.set_cell(coords, 0, Vector2i(2, 0), 0)
+	coords.y=2
+	foreground.set_cell(coords, 0, Vector2i(1, 0), 0)
+	coords.y=1
+	foreground.set_cell(coords, 0, Vector2i(0, 0), 0)
+	coords.y=0
+	foreground.set_cell(coords, 0, Vector2i(0, 1), 0)
+	print(foreground.get_cell_tile_data(coords))
